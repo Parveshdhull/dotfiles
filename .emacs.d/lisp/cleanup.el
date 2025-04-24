449ac6edc1009444389ff8e23d14c2f415df1b02
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2025-04-24 19:27:27 +0530

    Small changes: keybindings, cleanup etc.

--- .emacs.d/lisp/cleanup.el
+++ .emacs.d/lisp/cleanup.el
@@ -2,18 +2,18 @@
 (setq initial-scratch-message "")
 
 ;; Removes *scratch* from buffer after the mode has been set.
-(defun remove-scratch-buffer ()
-  (if (get-buffer "*scratch*")
-      (kill-buffer "*scratch*")))
-(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+;; (defun remove-scratch-buffer ()
+;;   (if (get-buffer "*scratch*")
+;;       (kill-buffer "*scratch*")))
+;; (add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
 
 ;; Removes *messages* from the buffer.
-(setq-default message-log-max nil)
-(kill-buffer "*Messages*")
+;; (setq-default message-log-max nil)
+;; (kill-buffer "*Messages*")
 
 ;; Removes *Completions* from buffer after you've opened a file.
-(add-hook 'minibuffer-exit-hook
-      '(lambda ()
-         (let ((buffer "*Completions*"))
-           (and (get-buffer buffer)
-                (kill-buffer buffer)))))
+;; (add-hook 'minibuffer-exit-hook
+;;       '(lambda ()
+;;          (let ((buffer "*Completions*"))
+;;            (and (get-buffer buffer)
+;;                 (kill-buffer buffer)))))
